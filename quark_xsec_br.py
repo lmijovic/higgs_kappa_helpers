2a7e24dcc770bdd62c54a32a8df83f4cc9545129
lmijovic <liza.mijovic@cern.ch>
2024-08-08 15:55:35 +0100

    add printouts for Giorgos benchmark scenarios

--- quark_xsec_br.py
+++ quark_xsec_br.py
@@ -2,32 +2,42 @@
 calculate xsec and BR for modified charm and u/d quark couplings
 '''
 
-
 import utils as utils
 
-#=======================================================================
-# 
-#=======================================================================
+
+print("\n\n----------------------------------------------------------\n")
+print("Standard Model:")
+kappabar_c = utils.kappabar_c_sm
+kappabar_ud = 0
+print("\nInclusive x-sections [pb]")
+print("ggH: ",utils.get_x_ggH_kappa(kappabar_c, kappabar_ud))
+print("ccH: ",utils.get_x_ccH_kappa(kappabar_c))
+print("uuH and ddH: ",utils.get_x_uuddH_kappa(kappabar_ud))
+print("\nH->yy branching ratio: [%]",
+      100.*utils.get_br_Hyy_kappa(kappabar_c, kappabar_ud))
+
+print("\n\n----------------------------------------------------------\n")
+print("Standard Model + Yukawa couplings: y_u = y_d = y_b")
+kappabar_c = utils.kappabar_c_sm
+kappabar_ud = 1
+print("\nInclusive x-sections [pb]")
+print("ggH: ",utils.get_x_ggH_kappa(kappabar_c, kappabar_ud))
+print("ccH: ",utils.get_x_ccH_kappa(kappabar_c))
+print("uuH and ddH: ",utils.get_x_uuddH_kappa(kappabar_ud))
+print("\nH->yy branching ratio: [%]",
+      100.*utils.get_br_Hyy_kappa(kappabar_c, kappabar_ud))
 
 
+print("\n\n----------------------------------------------------------\n")
+print("Standard Model + Yukawa couplings: y_u = y_d = y_c")
+kappabar_c = utils.kappabar_c_sm
+kappabar_ud = utils.kappabar_c_sm
+print("\nInclusive x-sections [pb]")
+print("ggH: ",utils.get_x_ggH_kappa(kappabar_c, kappabar_ud))
+print("ccH: ",utils.get_x_ccH_kappa(kappabar_c))
+print("uuH and ddH: ",utils.get_x_uuddH_kappa(kappabar_ud))
+print("\nH->yy branching ratio: [%]",
+      100.*utils.get_br_Hyy_kappa(kappabar_c, kappabar_ud))
 
-
-print("incusive x-sections at kappa_bar = 1: ")
-print("x_uuddH: ", x_uuddH, " [pb]")
-print("x_ccH: ", x_ccH, " [pb]")
-
-# ======================================================================
-# ggF cross-sections: affected by light quark loops
-# ======================================================================
-# let's leave everything to SM: 
-kappabar_c = 1/4.5 # mb_mc = 4.5 
-kappabar_ud = 0 
-sf_ggH = utils.get_x_sf_ggH_kappa(kappabar_c, kappabar_ud)
-print("inclusive standard model ggH x-section is: ", sf_ggH, utils.x_ggH_sm*sf_ggH, " pb")
-
-# let's set ud coupling equal to b coupling:  
-kappabar_c = 1/4.5 # mb_mc = 4.5 
-kappabar_ud = 1 
-sf_ggH = utils.get_x_sf_ggH_kappa(kappabar_c, kappabar_ud)
-print("inclusive ggH x-section is for kappabar_c:", kappabar_c, ", kappabar_ud:", kappabar_ud,
-      "is this time larger than SM:", sf_ggH, "and amounts to:", utils.x_ggH_sm*sf_ggH, " pb")
+# all done 
+print("\n\n")
